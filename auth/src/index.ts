5188fd78da89794d0b22785fc55ee52027dac7ea
dimakruglov <kruglovdmitrij@itclover.ru>
2022-04-16 18:19:46 +0300

    first commit

---
+++ auth/src/index.ts
@@ -0,0 +1,49 @@
+import express from 'express';
+import { json } from 'body-parser';
+import 'express-async-errors';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: true
+}));
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+app.use(errorHandler);
+
+const startup = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('jwt key not defined');
+  }
+
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('connected to mongodb');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000');
+  });
+}
+
+startup();
---
+++ auth/src/routes/signin.ts
@@ -0,0 +1,35 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { Password } from '../services/password';
+import { BadRequestError } from '../errors/bad-request-error';
+import { User } from '../models/user';
+import { validateRequest } from '../middlewares/validate-request';
+
+const router = express.Router();
+
+router.post('/api/users/signin', [
+  body('email').isEmail().withMessage('Email must be valid'),
+  body('password').trim().notEmpty().withMessage('Password required')
+], validateRequest, async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new BadRequestError('Invalid credentials');
+  }
+  
+  const passwordsMatch = await Password.compare(user.password, password);
+
+  if (!passwordsMatch) {
+    throw new BadRequestError('Invalid credentials');
+  }
+
+  const userJwt = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_KEY!);
+  req.session = { jwt: userJwt };
+  res.status(200).send(user);
+});
+
+export { router as signinRouter };
